0ffb61b49c53a59fb61456f6cb767a54b5c6d1a9
tunç <tunc.oruc59@gmail.com>
2024-06-16 01:09:57 +0300

    crafting system changed

--- OpenWorldSurvival/Assets/Scripts/CraftingSystem.cs
+++ OpenWorldSurvival/Assets/Scripts/CraftingSystem.cs
@@ -6,12 +6,28 @@ using UnityEngine;
 public class CraftingSystem : MonoBehaviour
 {
     [SerializeField] GameObject CraftingSystemScreenUI;
-    [SerializeField] GameObject ToolScreenUI;
     public static CraftingSystem instance;
     [SerializeField] List<GameObject> craftingSlotSlist;
+    private bool isopen=false;
+    [SerializeField] GameObject toolsScreenUI;
     private void Awake()
     {
         instance = this;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.T) && !isopen)
+        {
+            CraftingSystemScreenUI.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            isopen =true;
+        }
+        else if (Input.GetKeyUp(KeyCode.T) && isopen)
+        {
+            CraftingSystemScreenUI.SetActive(false);
+            Cursor.lockState = CursorLockMode.Locked;
+            isopen = false;
+        }
     }
     public void updateAllUI()
     {
@@ -19,6 +35,15 @@ public class CraftingSystem : MonoBehaviour
         {
             slot.GetComponent<CraftingSlot>().UpdateUI();
         }
+    }
+    public void openTools()
+    {
+       
+        toolsScreenUI.SetActive(true);
+    }
+    public void goBack()
+    {
+        toolsScreenUI.SetActive(false);
     }
     
 }
